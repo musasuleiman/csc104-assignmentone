68b266703e1d0816ae98c6f811f68d6d1466304e
musasuleiman <81877123+musasuleiman@users.noreply.github.com>
2021-04-04 08:43:16 -0700

    Add files via upload

---
+++ csc104assSectionB.py
@@ -0,0 +1,47 @@
+''' 
+   1. write a short python code to display your name amd department
+'''
+name = "Abdullahi Usman Musa"
+department = "computer science"
+print('Name:',name,"\n"'Department:',department)
+'''
+2. Assign the string 'boo!' to the variable scare and print the variable
+'''
+scare = 'boo!'
+print(scare)
+'''
+3. Add two variable and assign the result to a third varible.
+	print all the varible names 
+'''
+first_variable = 3
+second_variable = 867
+third_variable = 'first_variable '+ 'second_variable'
+print(first_variable, " ",second_variable, " " ,third_variable)
+'''
+4. divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable
+'''
+outcome = 12 / .5
+print(outcome)
+''' 
+5. assign the sum of two numbers to a variable and print the variable
+'''
+total = 50 + 50
+print(total)
+'''
+6. write the code that find a remainder when 8 is divided by 3
+ '''
+ first_atempt = 8 
+ second_atempt = 3
+ remainder = first_atempt % second_atempt
+ print(remainder)
+'''
+ 7. Rewrite the following statment to force this orders first, multiply 2 by 4. then add 4 and 2
+	then multiply the first result by second result print the result.
+	x = 2*4*4+2
+'''
+first_result = 2 * 4
+second_result = 4 + 2
+result = first_result * second_result
+print(result)
+
+
